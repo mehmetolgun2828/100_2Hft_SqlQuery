dc856a7f1a039399ac1accc5d6d0680f896f12dc
mehmetolgun2828 <93479293+mehmetolgun2828@users.noreply.github.com>
2022-08-07 17:15:40 +0200

    Add files via upload

---
+++ OneDrive_1_07-08-2022/Aggrgate_groupBy_0708.sql
@@ -0,0 +1,190 @@
+﻿
+
+-- AGGREGATE FUNCTION (toplam fonksiyonları, gruplamalı fonksiyonlar)
+
+--COUNT(*,kolonAd) =>  belirtilen satırdaki eleman sayısını sayar. SELECT komutundan sonra kullanılır.
+
+-- ProductID kolonunda kaç kayıt vardır?
+
+select COUNT(ProductID)
+from Products
+
+select * from Employees
+
+select COUNT(EmployeeID)
+from Employees
+
+select COUNT(*)
+from Employees
+
+select COUNT(Region)
+from Employees
+
+-- NOT => COUNT , boş geçilebilen alanları saymaz bu yüzden NOT NULL  olduğunu bildiğimz kolonlarda saydırm işlemi yapmak kesinlikle daha doğru bir sonuç verir.
+
+--çalışanlarımın kaç farklı şehirde yaşadığı bilgisini bulunuz.
+
+select  distinct City from Employees
+
+select  COUNT(distinct City) from Employees
+
+-- select 5+5 as toplam,67-89 as cıkarma
+
+
+--  SUM(kolonAd) => Verilen kolondaki değerleri toplar. Bu yüzden sadece sayısal kolonlarda iş yapar.
+
+--Toplam stok miktarımı bulunuz.
+
+select SUM(UnitsInStock) from Products
+
+-- çalışanlarımın yaşları toplamını bulunuz.
+
+select SUM(YEAR(GETDATE())-YEAR(BirthDate)) from Employees
+
+
+--AVG (kolonAd) => Verilen kolondaki sayısal degerlerin ortalama degerini bulur.
+
+-- çalışanlarımın ortalam yaşını bulunuz
+
+select AVG(YEAR(GETDATE())-YEAR(BirthDate)) from Employees
+
+
+
+------------------------------------------------------------
+
+
+-- GROUP BY  => Sorgu sonucunu verdiğimiz kolon bazında gruplayarak bize gruplu bir sonuç döner.
+-- ÇOK ÖNEMLİ NOT => GROUP BY kullanıyorsak SELECT ifadesinde listelenen sütunlar group by ifadesi tarafından kullanılmalı yani => groupby ve select beraber çalışmlaı. Çünkü grupladığımız tarfa aslınad bize tek bir veri döner buna dikkat etmeliyiz ve bu durumu büyük oranda AGGREGATE func ile göğüsleyebilirz.
+
+
+-- çalışanlarımı ülkelerine göre gruplayınız(hangi ülkeden kaç kiiş gelmekte gösteriniz)
+
+select Country,COUNT(*) as [Kişi sayısı]
+from Employees
+group by Country
+
+
+-- birim fiyatı 35 dolardan az olan ürünleri kategorilerine göre gruplayınız.(hangi kategoride kaç eleman var)
+
+select CategoryID,COUNT(*) as ToplamElemanSayısı
+from Products
+where UnitPrice<35
+group by CategoryID
+
+
+-- her bir siparişteki toplam ürün sayısını bulunuz.
+
+select * from  [Order Details]
+
+select OrderID,SUM(Quantity) as ToplamSiparisAdedi
+from [Order Details]
+group by OrderID
+
+-- her bir siparişimi toplam tutarına göre çoktan aza sıralayınız.
+
+select OrderID,sum(UnitPrice*Quantity*(1-Discount)) as ToplamTutar
+from [Order Details]
+group by OrderID
+order by  ToplamTutar desc
+
+
+----- HAVING => Sorgu sonucu gelen kümelerde Aggregate Func. bağlı olacak şekilde bir filtreleme yapılacaksa WHERE değil HAVING kullanılır. Eğer Agreeegate func. yok ise WHERE ile HVING aynı işi yapacaktır.
+
+-- Satır satır eleme yapılacaksa WHERE de koşul konu fakat kümeleme işlemleri sonucnda koşul varsa bu HAVING ile yapılır. İkisi beraberde tabiki kullanılabilinir.
+
+
+/*
+				MANTIKSAL İŞLEM SIRASI		    	AŞAMA
+							5                       SELECT
+							1						FROM            hangi tablo
+							2						WHERE           satır eleme
+							3						GROUP BY        kümeleme/gruplama
+							4						HAVING          grup üstünde işlem
+							6						ORDER BY        sıralama
+
+*/
+
+--- Toplam tutarı 2500 ile 3500 arasında olan siparişlerimi çoktan aza sıralayınız.
+
+select OrderID,sum(UnitPrice*Quantity*(1-Discount)) as Tutar
+from [Order Details]
+group by OrderID
+having sum(UnitPrice*Quantity*(1-Discount)) between 2500 and 3500
+order by Tutar desc
+
+
+--Her bir siparişteki toplam ürün adedi 200 den az olanları getirirniz.
+
+select OrderID,SUM(Quantity) as ToplamSiparisAdedi
+from [Order Details]
+group by OrderID
+having SUM(Quantity)<200
+order by ToplamSiparisAdedi desc
+
+
+--------------  SUB QUERY (içiçe sorgu)
+
+---fiyatı ortalama birim fiyatın üzerinde olan ürünlerimi getiirniz.
+
+select * from Products where UnitPrice>(select  AVG(UnitPrice) from Products)
+
+
+-- ürünler tablosunda satışını yaptığım ürünleri listeleyiniz.
+
+select * from Products where ProductID in (select ProductID from [Order Details])
+
+
+select CompanyName from Suppliers where SupplierID in (select SupplierID from Products where ProductID in (select ProductID from [Order Details]))
+
+--Romero y tomillo firmasına göndrdiğim ürünlerin siparişini hangi çalışanlarım onaylamıştır.
+
+select FirstName+SPACE(3)+LastName from Employees where EmployeeID in (select EmployeeID from Orders where CustomerID in (select CustomerID from Customers where CompanyName='Romero y tomillo'))
+
+--Kuzey bölgesinden sorumlu çalışanlarımın ad soyad title bilgsini getiriniz.
+
+
+select FirstName+SPACE(6)+LastName from Employees where EmployeeID in (select EmployeeID from EmployeeTerritories where TerritoryID in (select TerritoryID from Territories where RegionID=3))
+
+
+------------------------------------------------
+
+/*
+ JOIN farklı tabloları üzerinde bulunan ortak anahtarlar sayesinde birleştirip sanal tek bir tablo yaratmak işlemidir, kalıcı değildir. Sorgu sırasında kullanılır.
+
+	1 ) (INNER) JOIN => birleşen iki tablonun kesişiminde yer alan , boş verinin olmadığı satırlar oluşur.
+
+	2 )  (OUTER ) JOIN 
+
+		* LEFT (OUTER) JOIN 
+		*RIGHT (OUTER) JOIN
+
+		JOIN , sağ tarafında kalna tablo HEP RIGHT TABLE, solunda kalan tablo ise LEFT TABLE . LEFT joın yapıldığında soldaki tablonun tüm verileri diğer tablonun ise ilişkili verileri gelir . Aynı şekilde RIGHT join yapıldığında ise sağda kalan tablonun tüm verileri diğer tablonun ise ilişkli verileri gelir.
+
+*/
+
+-- ürünlerimin productName ve CategoryName bilgiisni getiiriniz.
+
+select ProductID,ProductName,CategoryName,[Description]
+from Products join Categories on Products.CategoryID=Categories.CategoryID
+
+
+---
+
+select CategoryName,ProductName,CompanyName,c.CategoryID 
+from Categories c join Products p on p.CategoryID=c.CategoryID
+                  join Suppliers s on s.SupplierID=p.SupplierID
+
+
+-----categoryNamelerine göre ürünlerin toplam stok miktarını bulunuz.
+
+select CategoryName,SUM(UnitsInStock) as toplamStok
+from Products p join Categories c on c.CategoryID=p.CategoryID
+group by CategoryName
+
+
+-- her bir çalışanın ne kadarlık satış yaptığını bulunuz.
+
+select FirstName+space(3)+LastName as TamAd,sum(UnitPrice*Quantity*(1-Discount)) as SatısTutarı,e.EmployeeID
+from Employees e join Orders o on o.EmployeeID=e.EmployeeID
+                 join [Order Details] od on od.OrderID=o.OrderID
+group by FirstName+space(3)+LastName,e.EmployeeID
---
+++ OneDrive_1_07-08-2022/MovieThaether_sorgular_06082022_5475.sql
@@ -0,0 +1,224 @@
+﻿
+--create database Filmler
+
+
+--CREATE table Categories
+--(
+-- CategoryId int identity(1,1) primary key,
+-- CategoryName nvarchar(60) not null
+--)
+
+--create table Seans
+--(
+--Seans int identity(1,1) primary key,
+--SeansSaat time not null
+--)
+
+--create table Weeks
+--(
+--WeekId int identity(1,1) primary key,
+--WeekStart datetime  not null,
+--WeekEnd datetime not null
+--)
+
+--create table Saloon
+--(
+--SalonId int identity(1,1) primary key,
+--SalonAd nvarchar(40) not null,
+--SalonKapasitesi int not null
+--)
+
+
+--create table Movies (
+--MovieId int identity(1,1) primary key,
+--MovieAd nvarchar(50) not null,
+--MovieAcıklaması nvarchar(90) not null,
+--MovieSure int not null
+
+--)
+
+--create table CategoryMovie
+--(
+--CategoryID int ,
+--MovieID int,
+--primary key (CategoryID,MovieID),
+--constraint fk_category foreign key(CategoryID)  references Categories(CategoryId),
+--constraint fk_movies foreign key(MovieID)  references Movies(MovieId)
+
+--)
+
+
+--create table MovieInfo
+--(
+--ID int identity(1,1) primary key,
+--SeansId int,
+--SalonID int,
+--WeekId int,
+--MovieId int,
+--constraint fk_seans foreign key(SeansId)  references Seans(Seans),
+--constraint fk_salon foreign key(SalonID)  references Saloon(SalonId),
+--constraint fk_week foreign key(WeekId)  references Weeks(WeekId),
+--constraint fk_moviess foreign key(MovieId)  references Movies(MovieId)
+--)
+
+
+------ TABLOLARI SORGULAMAK
+
+--select kolonAd1,kolonad2,.... FROM tabloAd
+--select *  from tabloAd (o tablonun tüm kolonlarını getirir)
+
+select * from Categories
+
+select CategoryID,CategoryName  from Categories
+
+
+-- Kolonların isimlendirilmesi
+
+select Title as Unvan,FirstName Ad, Soyad=LastName from Employees
+
+select Title ,FirstName , LastName from Employees
+
+
+-- Kolonların tek bir hücrede birleştirilmesi
+
+
+select  Title+'  '+FirstName+'  '+LastName as [Tam Ad] from Employees
+
+select  Title+SPACE(4)+FirstName+SPACE(4)+LastName as [Tam Ad] from Employees
+
+
+--Çalışanların şehirlerini getirniz.
+select City from Employees
+
+select distinct City from Employees
+
+
+--city bilgisi LOndon olanları getiriniz.
+  
+  select * from Employees where City='London'
+  select City,FirstName,LastName from Employees where City='London'
+
+
+-- Çalışanlarımdan Andrew Fuller rapor verenleri getiirniz.
+
+select ReportsTo,FirstName,LastName from Employees where ReportsTo=2
+
+-- Doğum yılı 1960 olan çalışanlarmı listeleyiniz.
+
+select * from Employees where year(BirthDate)=1960
+
+-- Doğum yılı 1950 ile 1960 arasında olanlar getiriniz.
+-- NOT => and (ve)  bağlanan ifadelerin ikisi de DOĞRU olmalı/sağlanmalı.
+--     => or  (veya/yada) bağlanan iki ifadeden birinin DOĞRU olması şartı sağlar.
+
+select * from Employees where YEAR(BirthDate)<=1960 and YEAR(BirthDate)>=1950
+
+-- Amerikada yaşayan Kadın çalışanlarımın ad, soyad,ülke bilgiisni getiirniz.
+select FirstName,LastName,Country,TitleOfCourtesy from Employees 
+where Country='USA' and (TitleOfCourtesy='Ms.' or TitleOfCourtesy='Mrs.')
+
+-- NULL VERİLERİ SORGULAMAK
+
+select Region,FirstName from Employees where  Region is null --  null olanlar gelsin
+
+select Region,FirstName
+from Employees
+where  Region is not  null -- null olmayanalr gelsin
+
+
+----- SIRALAMA İŞLEMLERİ - ORDER BY 
+
+--ASC(ASCENDİNG) yada DESC(DESCENDING) olarak sıralama yapılabiliriz.
+-- ASC => azdan çoka sıralar (sayısal degerlerde), a -> z HARFİNE GÖRE SIRAALAR (metinsel ifadede),geçmişten -> geleceğe (yakın tarihe) göre sıralar (tarihsel ifadelerde.)
+
+-- NOT => AKSİ SÖYLENMEDİĞİ SÜRECE ASC olarak sıralandığı kabu edilir.
+
+-- ÜRÜNLERİMİ ıd bilgilerine göre çoktan aza sıralayınız.
+
+select * 
+from Products
+where ProductID<=8 and ProductID>=5
+order by ProductID desc
+
+
+select * 
+from Products
+where ProductID<=8 and ProductID>=5
+order by ProductName 
+
+
+select Title,LastName,FirstName,HireDate,TitleOfCourtesy 
+from Employees
+order by 2,4 desc
+
+-- NOT => order by ifaedesindeki sayısal kolonlar aslında SELECT ifadesşinde seçilen kolon adlarının 1den başlayarak sıralmasıdır. YUlarıdaki örnekte order by da LastName kolonuna göre ASC ve aynı veriyle karşılaşıldğında 4. sıradaki HİREdATE KOLONUNA GÖRE desc OLARAK SIRALAMA yapılacaktır demektir.
+
+
+--- çalışanlarımı yaşlarına göre çoktan aza sıralayınız.
+
+select DATEDIFF(YEAR,BirthDate,GETDATE()) as yas
+from Employees
+order by yas desc
+
+select FirstName,LastName,YEAR(GETDATE())-YEAR(BirthDate) as Yas
+from Employees
+order by Yas desc
+
+
+--SELECT FirstName, LastName,BirthDate from Employees ORDER BY year(BirthDate) 
+
+
+---alfabetik olarak çalışanlarımdan robert ve janet arasında kalanları isimlerine göre çoktan aza sıralayınız.
+
+select FirstName, LastName
+ From Employees
+ where FirstName >= 'Janet' and FirstName <= 'Robert'
+ order by 1  desc
+
+ select FirstName, LastName 
+ From Employees
+ where FirstName between 'Janet' and 'Robert'
+ order by 1  desc
+
+ --not => BETWEEN de sınırlar dahil kabul edilir.
+
+
+ -- Ünvanı DR. yada Mr. olan çalışanlarımı getiirniz.
+
+ select *
+ from Employees
+ where TitleOfCourtesy='Dr.' or  TitleOfCourtesy='Mr.'
+
+
+select *
+from Employees
+where TitleOfCourtesy in ('Dr.','Mr.')
+
+
+-- En pahalı 3 ürünümü getiriniz.
+
+select top 3*  -- tüm kolonlarını getirir.
+from Products
+order by UnitPrice desc
+
+select top 3 UnitPrice,ProductName,ProductID  -- seçilen kolonlarını getirir.
+from Products
+order by UnitPrice desc
+
+--benimle beraber çalışan en eski 5 çalışanımı getiriniz.
+
+select top 5 FirstName,LastName,YEAR(GETDATE())- YEAR(HireDate) as KacYıldırCalısıyor 
+from Employees
+order by HireDate 
+
+-- LIKE  KULLANMI
+
+-- ADININ BAŞ HARFİ A OLANLAR
+SELECT * FROM Employees where FirstName like 'A%'
+
+-- ADIIN İÇİNDE A HARFİ OLANLAR
+
+SELECT * FROM Employees where FirstName like '%A%'
+
+-- ADININ SON HARFİ A OLANLAR
+SELECT * FROM Employees where FirstName like '%A'
